8e0c4d46465a06e8bb7412c97d30c94e213d0355
alich6 <alicheraghi148@gmail.com>
2023-02-17 19:51:47 +0330

    add hooks

---
+++ src/hooks/useUserRole.ts
@@ -0,0 +1,11 @@
+/**
+ * this hooks get user role and return it
+ */
+
+export const useUserRole = (): UserAccessRole => {
+  // somehow get user and extract role
+
+  const role = 'admin'
+
+  return role
+}
